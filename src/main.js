927e257bd94014e36153f70500fda8b0eba28eea
jeremy <wallslide@users.noreply.github.com>
2019-06-09 18:57:44 +0900

    Update package.json with correct metadata and build script. Update main script to export component and enum

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-import './plugins/vuetify'
+import ImagePicker from '@/components/image-picker'
+import { imageUploadingStates } from '@/components/image-picker/enums'
+
+export { ImagePicker, imageUploadingStates }
